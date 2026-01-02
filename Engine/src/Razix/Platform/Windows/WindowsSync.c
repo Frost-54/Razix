0dad79831beb1dd6dd37c675e615a84ad7ebe122
Frost54 <91168148+Frost-54@users.noreply.github.com>
2026-01-02 22:03:06 +1000

    Use SRWLOCK because it is newer and more performant

--- Engine/src/Razix/Platform/Windows/WindowsSync.c
+++ Engine/src/Razix/Platform/Windows/WindowsSync.c
@@ -14,31 +14,29 @@ rz_critical_section rz_critical_section_create_ex(u32 spintCount)
 {
     rz_critical_section cs;
     cs.m_SpinCount = spintCount;
-    bool ret       = InitializeCriticalSectionAndSpinCount((CRITICAL_SECTION*) &cs.m_Internal.buffer, cs.m_SpinCount);
-    RAZIX_CORE_ASSERT(ret, "Failed to initialize Critical Section with spintcount!");
-    RAZIX_UNUSED(ret);
+    InitializeSRWLock((PSRWLOCK) &cs.m_Internal.buffer);
 
     return cs;
 }
 
 void rz_critical_section_destroy(rz_critical_section* cs)
 {
-    DeleteCriticalSection((CRITICAL_SECTION*) &cs->m_Internal.buffer);
+
 }
 
 void rz_critical_section_lock(rz_critical_section* cs)
 {
-    EnterCriticalSection((CRITICAL_SECTION*) &cs->m_Internal.buffer);
+    AcquireSRWLockExclusive((PSRWLOCK) &cs->m_Internal.buffer);
 }
 
 bool rz_critical_section_try_lock(rz_critical_section* cs)
 {
-    return TryEnterCriticalSection((CRITICAL_SECTION*) &cs->m_Internal.buffer) != 0;
+    return TryAcquireSRWLockExclusive((PSRWLOCK) &cs->m_Internal.buffer) != 0;
 }
 
 void rz_critical_section_unlock(rz_critical_section* cs)
 {
-    LeaveCriticalSection((CRITICAL_SECTION*) &cs->m_Internal.buffer);
+    ReleaseSRWLockExclusive((PSRWLOCK) &cs->m_Internal.buffer);
 }
 
 //-----------------------------------------------------------------------------
@@ -67,11 +65,11 @@ void rz_conditional_var_broadcast(rz_cond_var* cv)
 
 void rz_conditional_var_wait(rz_cond_var* cv, rz_critical_section* cs)
 {
-    SleepConditionVariableCS((CONDITION_VARIABLE*) &cv->m_Internal.buffer, (CRITICAL_SECTION*) &cs->m_Internal.buffer, INFINITE);
+    SleepConditionVariableSRW((CONDITION_VARIABLE*) &cv->m_Internal.buffer, (PSRWLOCK) &cs->m_Internal.buffer, INFINITE);
 }
 
 void rz_conditional_var_wait_until(rz_cond_var* cv, rz_critical_section* cs, u32 timeout_ms)
 {
-    SleepConditionVariableCS((CONDITION_VARIABLE*) &cv->m_Internal.buffer, (CRITICAL_SECTION*) &cs->m_Internal.buffer, (DWORD) timeout_ms);
+    SleepConditionVariableSRW((CONDITION_VARIABLE*) &cv->m_Internal.buffer, (PSRWLOCK) &cs->m_Internal.buffer, (DWORD) timeout_ms);
 }
 #endif
